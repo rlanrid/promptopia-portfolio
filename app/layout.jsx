0c32064bda5d50a61f9ecbeb3f949210551c58a1
rlanrid <rladnrider77@gmail.com>
2024-01-18 17:27:31 +0900

    배포

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
 export const metadata = {
-    title: "Layout",
+    title: "Promptopia",
     description: 'Discover & Share AI Prompts'
 }
 
